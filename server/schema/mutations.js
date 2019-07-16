b52ff81e3b6a901dbf9cba21c8692b19f1043575
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-07-16 08:54:46 -0700

    add email/pw args to resolve function

---
+++ server/schema/mutations.js
@@ -0,0 +1,35 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString
+} = graphql;
+const UserType = require('./types/user_type');
+
+const mutations = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        signup: {
+            type: UserType,
+            args: {
+                email: {
+                    type: GraphQLString
+                },
+                password: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parentValue, {
+                email,
+                password
+            }, req) {
+                AuthService.signup({
+                    email,
+                    password,
+                    req
+                })
+            }
+        }
+    }
+});
+
+module.exports = mutations;
